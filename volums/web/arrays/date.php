a530237b4091c92edc0d536aa15ad0927e2b2904
JohnCast867 <johnjavier867@gmail.com>
2023-11-22 15:43:48 +0100

    so

---
+++ volums/web/arrays/date.php
@@ -0,0 +1,6 @@
+<html>
+<?php
+$fecha = date("m");
+?>
+
+</html>
--- volums/web/prova/lectura_json_array.php
+++ volums/web/prova/lectura_json_array.php
@@ -3,18 +3,61 @@
 <body>
 <?php
 
-$jsonString = file_get_contents('prova_json.json');
+//exercici 1
+function carrega_fitxer($nomfitxer, &$arrayAsociatiu2) {
+    $jsonString = file_get_contents($nomfitxer);
+    $arrayAsociatiu2 = json_decode($jsonString, true);
+    
+    if (json_last_error() !== JSON_ERROR_NONE) {
+        die('Error  JSON: ' . json_last_error_msg());
+    }
+}
 
+$persones = array();
+carrega_fitxer("prova_json.json", $persones);
 
-$arrayAsociatiu = json_decode($jsonString, true);
+print_r($persones);
 
-// Verifica si hay errores durante la decodificación
-if (json_last_error() !== JSON_ERROR_NONE) {
-    die('Error  JSON: ' . json_last_error_msg());
+//exercici 2
+function imprimir_array ($array) {
+    echo "<table>";
+    echo '<tr><td>'."Nom".'</td>';
+    echo '<td>'."Edat".'</td>';
+    echo '<td>'."Ciutat".'</td></tr>';
+    foreach ($array as $persona => $value){
+             echo "<tr><td>". $value['nom']."</td>
+             <td>".$value ['edat']."</td>
+            <td>" . $value['ciutat'] ."</td></tr>";
+             }  
+             echo "</table>";
+                  echo '<style type="text/css">';
+                  echo "table { border: 2px solid black } ";
+                  echo "td{ border: 2px solid black;} ";
 }
 
 
-print_r($arrayAsociatiu);
+
+// echo "<table>";
+// echo '<tr><td>'."Nom".'</td>';
+// echo '<td>'."Edat".'</td>';
+// echo '<td>'."Ciutat".'</td></tr>';
+// foreach ($arrayAsociatiu as $persona => $value){
+//     echo "<tr><td>". $value['nom']."</td>
+//     <td>".$value ['edat']."</td>
+//     <td>" . $value['ciutat'] ."</td></tr>";
+//     }
+//     echo "</table>";
+//     echo '<style type="text/css">';
+//     echo "table { border: 2px solid black } ";
+//     echo "td{ border: 2px solid black;} ";
+
+
+//exercici 3
+
+function recuperar_nom_persones($array,&$noms) {
+
+}
+
 
 
 ?> 
