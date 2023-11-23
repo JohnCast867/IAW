2918579a178c0af63617b23ce7e6e83545408199
nicollanca <nagmansilla2002@gmail.com>
2023-11-23 17:34:56 +0100

    si

--- volums/web/Proyecte/funcion1.php
+++ volums/web/Proyecte/funcion1.php
@@ -1,30 +1,6 @@
 <?php
-function carrega_fitxer($nomFitxer, &$arrayAsso) {
-    $jsonString = file_get_contents($nomFitxer);
-    $arrayAsso = json_decode($jsonString, true);
-    
-    // Verifica si hay errores durante la decodificación
-    if (json_last_error() !== JSON_ERROR_NONE) {
-        die('Error  JSON: ' . json_last_error_msg());
-    }
-}
-
+include "funciones.php";
 $juegos = array();
 carrega_fitxer("games.json", $juegos);
-function tabla ($array)
-{
-echo "<table border=1>";
-echo '<tr><th>Nom</th>
-    <th>Desenvolupador</th>
-    <th>Plataforma</th>
-    <th>Llançament</th></tr>';
-foreach ($array as $key => $value){
-    echo "<tr><td>" . $value['Nom'] . "</td>
-    <td>" . $value['Desenvolupador'] . "</td>
-    <td>" . $value['Plataforma'] . "</td>
-    <td>" . $value['Llançament'] . "</td></tr>";
-    }
-    echo "</table>";
-        }
 tabla($juegos);
 ?>
