42fc0158969814d368e2ee339175ec9aa61bf90a
nicollanca <nagmansilla2002@gmail.com>
2023-11-23 17:44:03 +0100

    A

--- volums/web/Proyecte/proyecto.php
+++ volums/web/Proyecte/proyecto.php
@@ -7,7 +7,14 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+    <h1>Proyecto 1era avaluacion</h1>
+    <h2>John javier castillo y Nicolas Gimenez Mansilla</h2>
+    <nav>
+        <h4>Funcionalidad 1</h4>
+        <h4>Funcionalidad 2</h4>
+        <h4>Funcionalidad 3</h4>
+        <h4>Funcionalidad 4</h4>
+    </nav>
 </body>
 <?php
     include 'funcion1.php';
