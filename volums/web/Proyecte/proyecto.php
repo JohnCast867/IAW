d9a8a1df5f71d015f930d7414814211c6ef94cd3
nicollanca <nagmansilla2002@gmail.com>
2023-11-23 17:21:01 +0100

    waza

---
+++ volums/web/Proyecte/proyecto.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina principal</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+</body>
+<?php
+    include 'funcion1.php';
+    tabla($juegos);
+?>
+</html>
