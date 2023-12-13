dd304831c7ea99d79992d098832e74938a06fa7d
JohnCast867 <johnjavier867@gmail.com>
2023-12-13 16:32:31 +0100

    si

--- volums/web/Proyecte/funcion4.php
+++ volums/web/Proyecte/funcion4.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina principal</title>
+    <title>Funcion 4</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
