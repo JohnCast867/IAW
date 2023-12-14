78d1e61c11a8619745ac37242cf3a17810548334
JohnCast867 <johnjavier867@gmail.com>
2023-12-14 15:27:01 +0100

    sss

---
+++ volums/web/CASTILLO_VILLALTA_JOHN/act1.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <h1>Exercici 1</h1>
+
+<?php
+
+function filtrarPerAnyNaixement($datesNaixement, $noms, $anyInici, $anyFi) {
+    $personesFiltrades = array();
+
+    foreach ($datesNaixement as $dni => $dataNaixement) {
+        $anyNaixement = date("Y", strtotime($dataNaixement));
+
+        if ($anyNaixement >= $anyInici && $anyNaixement <= $anyFi) {
+            $personesFiltrades[$dni] = $noms[$dni];
+        }
+    }
+
+    return $personesFiltrades;
+}
+
+$datesNaixement = array(
+    "123456789" => "1990-05-15",
+    "987654321" => "1985-08-22",
+    "456789012" => "1998-03-10",
+    "321098765" => "1976-11-28",
+    "789012345" => "2002-07-05",
+    "543210987" => "1994-02-18",
+    "210987654" => "1980-09-03",
+    "876543210" => "2005-12-12",
+    "234567890" => "1999-06-25",
+    "890123456" => "1972-04-07"
+);
+
+$noms = array(
+    "123456789" => "Mora Barceló, Joana",
+    "456789012" => "Barceló Adrover, Maria",
+    "321098765" => "Sànchez López, Margalida",
+    "789012345" => "Oliver Montserrat, Paula",
+    "210987654" => "Mora Llaneres, Falou",
+    "876543210" => "Nadal Barceló, Miquel",
+    "234567890" => "Sastre Julià, Joana",
+    "543210987" => "Mora Ferrà, Toni",
+    "987654321" => "Nicolau Martorell, Xim",
+    "890123456" => "Bibiloni Sagreres, Bàrbara"
+);
+
+$anyInici = 1990;
+$anyFi = 2010;
+$resultat = filtrarPerAnyNaixement($datesNaixement, $noms, $anyInici, $anyFi);
+
+echo "Persones nascudes entre els anys $anyInici i $anyFi:\n";
+print_r($resultat);
+
+?>
+
+
+</body>
+
+</html>
