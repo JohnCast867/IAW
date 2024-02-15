6c53266cbff7c85fc43f340e2a62e422a8689faf
JohnCast867 <johnjavier867@gmail.com>
2024-02-15 15:14:44 +0100

    aaa

--- volums/web/Proyecte2/db.php
+++ volums/web/Proyecte2/db.php
@@ -4,7 +4,7 @@
    $password = "politecnic";
    
    try {
-     $conn = new PDO("mysql:host=$servername;dbname=VIDEOJOCS", $username, $password);
+     $conn = new PDO("mysql:host=$servername", $username, $password);
      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
      echo "Connected successfully";
    } catch(PDOException $e) {
